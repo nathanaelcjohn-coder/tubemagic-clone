6e2adc28d7ba12dd0b113bbf6120f408b6ffbfd9
nathanaelcjohn-coder <nathanaelcjohn@gmail.com>
2026-03-29 18:35:06 +0530

    Redeploy

--- middleware.ts
+++ middleware.ts
@@ -1,9 +1,12 @@
-import { authMiddleware } from "@clerk/nextjs";
+import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 
-export default authMiddleware({
-  publicRoutes: ["/"], // Homepage is public, everything else is locked
+// This locks the dashboard and any sub-pages
+const isProtectedRoute = createRouteMatcher(['/dashboard(.*)']);
+
+export default clerkMiddleware((auth, req) => {
+  if (isProtectedRoute(req)) auth().protect();
 });
 
 export const config = {
-  matcher: ["/((?!.+\\.[\\w]+$|_next).*)", "/", "/(api|trpc)(.*)"],
+  matcher: ["/((?!.*\\.[\\w]+$|_next).*)", "/", "/(api|trpc)(.*)"],
 };
