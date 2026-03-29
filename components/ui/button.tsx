2bd90cf0b6b2aaccdd2f040ac2df61f750d9aa45
nathanaelcjohn-coder <nathanaelcjohn@gmail.com>
2026-03-29 21:10:49 +0530

    clerk v7

--- components/ui/button.tsx
+++ components/ui/button.tsx
@@ -40,16 +40,15 @@ export interface ButtonProps
 }
 
 const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
-  ({ className, variant, size, asChild = false, ...props }, ref) => {
-    const Comp = asChild ? Slot : 'button';
-    return (
-      <Comp
-        className={cn(buttonVariants({ variant, size, className }))}
-        ref={ref}
-        {...props}
-      />
-    );
-  }
+  function ({ className, variant, size, asChild = false, ...props }, ref) {
+  const Comp = asChild ? Slot : 'button';
+  return (
+    <Comp
+      className={cn(buttonVariants({ variant, size, className }))}
+      ref={ref}
+      {...props} />
+  );
+}
 );
 Button.displayName = 'Button';
 
